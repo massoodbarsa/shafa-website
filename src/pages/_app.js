c1802def731a5c6a78f215db1c1599143f04148f
amir.shakiba1980@gmail.com <johndoe@example.com>
2025-02-09 15:43:08 +0100

    protect admin route

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "../styles/theme";
 import Layout from "../components/Layout"; // Your custom layout component
 
 import { SnackbarProvider } from "notistack";
+import { useRouter } from "next/router";
+import useAuthStore from "../store/authStore";
+import { UserRole } from "@/enums/UserRole";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  const { user } = useAuthStore();
+
+  console.log(router);
+  //Protect admin route
+  useEffect(() => {
+    const checkUserRole = async () => {
+      if (router.pathname.includes("/admin") && user?.role !== UserRole.Admin) {
+        router.push("/"); // Redirect to homepage if not admin
+      }
+    };
+
+    checkUserRole();
+  }, [router.pathname]);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline /> {/* Normalize CSS */}
