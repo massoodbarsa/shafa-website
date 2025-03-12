563ff4593816b117994c30a55eb4ad55caa5c1fa
Amir Shakiba <amir.shakiba1980@gmail.com>
2025-03-12 16:00:22 +0100

    add -documet

---
+++ src/pages/_document.js
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Favicon Link */}
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          {/* Optional: You can add multiple sizes/formats for better compatibility */}
+          <link rel="icon" href="/favicon.png" type="image/png" sizes="32x32" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
