8699a568f38a8cf59ec7001af06c8227f1db8da3
Amir Shakiba <amir.shakiba1980@gmail.com>
2025-03-24 15:18:58 +0100

    seo changes home

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,13 +45,13 @@ const Homepage = () => {
         title="Find Farsi-speaking Doctors for Iranians Abroad"
         description="Our platform connects Iranians living outside of Iran with Farsi-speaking doctors, making healthcare more accessible and personalized."
         openGraph={{
-          url: "https://www.yourwebsite.com", // Replace with your homepage URL
-          title: "Find Farsi-speaking Doctors for Iranians Abroad",
+          url: "https://www.iraniandoctorshub.com", // Replace with your homepage URL
+          title: "Find Farsi-speaking doctors for Iranians abroad",
           description:
             "Our platform connects Iranians living outside of Iran with Farsi-speaking doctors, making healthcare more accessible and personalized.",
           images: [
             {
-              url: "https://www.yourwebsite.com/images/hero-banner.jpg", // Replace with your image URL
+              url: "https://www.iraniandoctorshub.com/logo.jpg", // Replace with your image URL
               width: 1200,
               height: 630,
               alt: "Doctors Connecting with Iranians Abroad",
@@ -67,7 +67,6 @@ const Homepage = () => {
           logo: "https://www.iraniandoctorshub.com/logo.png", // Replace with your logo URL
           contactPoint: {
             "@type": "ContactPoint",
-            telephone: "+31-123-456-7890", // Replace with your contact number
             contactType: "customer service",
             areaServed: "Worldwide", // You can change this to specific countries or regions
             availableLanguage: "Farsi, English", // Add languages available for customer support
